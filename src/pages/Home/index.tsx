91e28a29fb69befc74868bd79ba422233fd356c3
Gabriel <gabriel1997.castro@gmail.com>
2022-04-22 15:25:14 -0300

    show products from the api

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -22,16 +22,28 @@ interface CartItemsAmount {
 }
 
 const Home = (): JSX.Element => {
-  // const [products, setProducts] = useState<ProductFormatted[]>([]);
-  // const { addProduct, cart } = useCart();
+  const [products, setProducts] = useState<ProductFormatted[]>([]);
+  const { addProduct, cart } = useCart()
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    if (sumAmount[product.id]) {
+      sumAmount[product.id]++
+    } else sumAmount[product.id] = 1
+    return sumAmount
+  }, {} as CartItemsAmount)
 
   useEffect(() => {
     async function loadProducts() {
-      // TODO
+      api.get('/products')
+        .then((res) => {
+          const { status } = res
+          if (status >= 200 && status < 300) {
+            const formattedData: ProductFormatted[] = res.data.map((item: Product) => {
+              return { ...item, priceFormatted: formatPrice(item.price) }
+            })
+            setProducts(formattedData)
+          }
+        })
     }
 
     loadProducts();
@@ -43,23 +55,25 @@ const Home = (): JSX.Element => {
 
   return (
     <ProductList>
-      <li>
-        <img src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg" alt="Tênis de Caminhada Leve Confortável" />
-        <strong>Tênis de Caminhada Leve Confortável</strong>
-        <span>R$ 179,90</span>
-        <button
-          type="button"
-          data-testid="add-product-button"
-        // onClick={() => handleAddProduct(product.id)}
-        >
-          <div data-testid="cart-product-quantity">
-            <MdAddShoppingCart size={16} color="#FFF" />
-            {/* {cartItemsAmount[product.id] || 0} */} 2
-          </div>
+      {products.map(product => (
+        <li key={product.id}>
+          <img src={product.image} alt="Tênis de Caminhada Leve Confortável" />
+          <strong>{product.title}</strong>
+          <span>{product.priceFormatted}</span>
+          <button
+            type="button"
+            data-testid="add-product-button"
+          // onClick={() => handleAddProduct(product.id)}
+          >
+            <div data-testid="cart-product-quantity">
+              <MdAddShoppingCart size={16} color="#FFF" />
+              {cartItemsAmount[product.id] || 0}
+            </div>
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+      ))}
     </ProductList>
   );
 };
